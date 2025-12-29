7a0ebacf20bc6bec0ed3999706b9d8e94022495e
akshayg-code <agundigara@sigmasolve.com>
2025-12-29 23:09:00 +0530

    Build

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,15 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  // server: {
+  //   host: "::",
+  //   port: 8080,
+  // },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  base: "/ardent-clinic-refresh/",
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./src"),
+  //   },
+  // },
 }));
